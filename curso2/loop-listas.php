c3495da8bd50f01ce0f92fcc9ad49700b4b77578
Estevão <estevao.aztex@gmail.com>
2023-01-08 12:20:45 -0300

    sei la

--- curso2/loop-listas.php
+++ curso2/loop-listas.php
@@ -1,9 +1,7 @@
 <?php
 
-$idadeList = [21, 23, 19, 25, 30, 41, 18];
+$idadeList = [23, 19, 25, 30, 41, 18, 21, 35];
 
-echo "O tamanho da lista é:" . sizeof($idadeList);
-
-for ($i = 0; $i < 7; $i++) {
+for ($i = 0; $i < count($idadeList); $i++) {
     echo $idadeList[$i] . PHP_EOL;
 }
